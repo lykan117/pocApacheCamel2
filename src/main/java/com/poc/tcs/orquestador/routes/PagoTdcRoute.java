1dd5c63c527e9a58e169d1844bc802c738b03721
Edson GO <lyancane93@gmail.com>
2025-05-20 11:31:58 -0600

    5 commit

--- src/main/java/com/poc/tcs/orquestador/routes/PagoTdcRoute.java
+++ src/main/java/com/poc/tcs/orquestador/routes/PagoTdcRoute.java
@@ -1,20 +1,43 @@
 package com.poc.tcs.orquestador.routes;
 
 import com.poc.tcs.orquestador.util.EncryptionUtils;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
+
+import java.net.ConnectException;
 
 @Component
 public class PagoTdcRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
+
+//        onException(Exception.class)
+//                .maximumRedeliveries(3)
+//                .redeliveryDelay(2000)
+//                .retryAttemptedLogLevel(LoggingLevel.WARN)
+//                .handled(true)
+//                .to("kafka:pagos.tdc.dlq");
+        onException(ConnectException.class)
+                .maximumRedeliveries(5)
+                .redeliveryDelay(3000)
+                .retryAttemptedLogLevel(LoggingLevel.WARN)
+                .handled(true)
+                .to("kafka:pagos.tdc.retry");
+
+        onException(RuntimeException.class)
+                .maximumRedeliveries(1)
+                .handled(true)
+                .to("kafka:pagos.tdc.dlq");
+
+
         from("direct:inicioPago")
                 .process(exchange -> {
                     String tarjeta = exchange.getIn().getHeader("numTarjeta", String.class);
                     String cifrada = EncryptionUtils.encrypt(tarjeta);
                     exchange.getIn().setHeader("numTarjeta", cifrada);
                 })
-                .to("kafka:pagos.tdc.request?brokers=localhost:9092")
+                .to("kafka:pagos.tdc.request")
                 .to("direct:procesarPago");
 
         from("direct:procesarPago")
@@ -29,12 +52,17 @@ public class PagoTdcRoute extends RouteBuilder {
                 .to("http://localhost:8081/servicio/pago")
                 .onFallback()
                 .log("Fallback activado: servicio de pago no disponible")
-                .to("kafka:pagos.tdc.compensacion?brokers=localhost:9092")
+                .to("kafka:pagos.tdc.compensacion")
                 .end()
                 .multicast().parallelProcessing()
                 .to("direct:notificar", "direct:auditar")
                 .end()
                 .to("kafka:pagos.tdc.ok?brokers=localhost:9092");
+
+        from("kafka:pagos.tdc.compensacion")
+                .log("Compensando pago fallido: ${body}")
+                .to("bean:compensacionService?method=compensar");
+
 
 
 
